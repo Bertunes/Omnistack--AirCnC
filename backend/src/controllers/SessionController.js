cde9efca8b641259116e4fda86999eb04d6a64bb
Bertunes <tharlleybertunes@gmail.com>
2019-10-03 03:03:58 -0300

    AirCnC

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,20 @@
+//index(listagem de sessão),// show(listar unica sessão), store(criar sessão), update(alterar sessão), destroy(deletar sessão)
+
+const User = require('../models/User')
+
+module.exports = {
+    async store(req, res){
+       const {email} = req.body;
+
+       let user =await User.findOne({email});
+
+       if (!user){
+
+        user = await User.create({email})
+       }
+
+
+       return res.json(user);
+    }
+
+};
